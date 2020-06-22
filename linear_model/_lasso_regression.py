0663cef3e9fcd6f10a25aa5af6e5fb8c553bb72c
fffffarmer <fffffarmer@sjtu.edu.cn>
2020-06-22 21:20:26 +0800

    update lasso

--- linear_model/_lasso_regression.py
+++ linear_model/_lasso_regression.py
@@ -17,75 +17,103 @@ class LassoRegression():
     
     def fit(self, X, y, verbose=False):
         num_samples, num_feats = X.shape
-        min_loss = np.inf
-        count_no_change = 0
         self._w = random.uniform(low=-1, high=1, size=(num_feats, ))
-        
-        while count_no_change < self._n_iter_no_change:
-            # TODO: a better way
-            if count_no_change >= 10:
-                self._learning_rate /= 1.001
-                
+
+        # TODO: max_iter
+        for k in range(100):
             assert num_samples >= self._batch_size
             sampled = random.choice(num_samples, self._batch_size, replace=False)
-            loss = self._cal_lasso_loss(X[sampled, :], y[sampled], self._w, self._alpha)
             
             if verbose:
-                print('Loss:\t{:.4f}'.format(loss))
-                        
-            if loss > min_loss - self._tolerance:
-                count_no_change += 1
-            else:
-                count_no_change = 0
-                
-            min_loss = min(min_loss, loss)
-            
-            # Updates
-            self._w = self._coordinate_descent_step(X[sampled, :], y[sampled], self._w, self._alpha)
+                print('Loss:\t{:.4f}'.format(self._cal_loss(X[sampled, :], y[sampled], self._w)))
+
+            self._w = self._IRLS(X[sampled, :], y[sampled], self._w, self._alpha)
         
     def predict(self, X, threshold=0.5):
         assert self._w is not None
         
-        return 1 / (1 + np.exp(-1 * np.dot(X, self._w))) > threshold
-        
+        return sigmoid(np.dot(X, self._w)) > threshold
+    
     @staticmethod
-    def _cal_lasso_loss(X, y, w, alpha):
+    def _cal_grad_1d(X, y, w, alpha, dim):
         num_samples, _ = X.shape
         
         proj = np.dot(X, w)
-        loss = np.sum(np.log(1 + np.exp(proj))) - np.dot(y, proj) + alpha * LA.norm(w, ord=1)
+        grad_1d = np.dot(X[:, dim], 1 - 1 / (1 + np.exp(proj))) - np.dot(y, X[:, dim]) + alpha * np.sign(w[dim])
         
-        return loss / num_samples
+        return grad_1d / num_samples
     
     @staticmethod
-    def _coordinate_descent_step(X, y, w, alpha):
-        """Update all coordinates under some fix alpha
-
-            The calculation process is refered to 'Efficient L1 Regularized Logistic Regression'
+    def _cal_loss(X, y, w):
+        num_samples, _ = X.shape
+        
+        proj = np.dot(X, w)
+        loss = np.sum(np.log(1 + np.exp(proj))) - np.dot(y, proj)
+        
+        return loss / num_samples
+     
+    @staticmethod
+    def _coordinate_descent_least_squares(X, y, w, init_alpha):
+        """Use coordinate descent to solve the L1 constrained least squares problem.
+        """ 
+        _, num_feats = X.shape
+        
+        # TODO: a better way
+        for alpha in np.linspace(init_alpha, init_alpha / 5, 10):
+            for dim in range(num_feats):
+                r = y - np.dot(X, w) + X[:, dim] * w[dim]
+                norm = LA.norm(X[:, dim]) ** 2
+                gamma = np.dot(r, X[:, dim])
+                
+                w[dim] = np.sign(gamma) * max(0, (abs(gamma) - alpha) / norm)
+        
+        return w
+    
+    def _IRLS(self, X, y, w, init_alpha):
+        """Solve the L1 Regularized Logistic Regression by IRLS-coordinate.
+        
+            This solution is inspired by 'Efficient L1 Regularized Logistic Regression',
+            where LARS is used instead of coordinate descent method.
         """ 
         num_samples, num_feats = X.shape
         
         proj = np.dot(X, w)
-        y_est = sigmoid(proj)
-        d = y_est * (1 - y_est)
-        D = np.diag(y_est * (1 - y_est))
+        predicted = sigmoid(proj)
+        
+        d = predicted * (1 - predicted)
+        # The diagonal matrix (square-rooted)
+        D = np.diag(d ** 0.5)
+        # The vector
         z = proj + (1 - sigmoid(y * proj)) * y / d
         
-        # TODO: 
+        # Solve the sub L1 constrained least squares problem
+        w_pseudo = np.zeros(num_feats)
+        X_pseudo = np.dot(D, X)
+        y_pseudo = np.dot(D, z)
         
-        # for j in range(num_feats):
-        #     proj = np.dot(X, w)
-        #     y_est = 1 / (1 + np.exp(-1 * proj))
-        #     W = np.diag((y_est * (1 - y_est)))
-        #     X_j = X[:, j]
-            
-        #     v_j = (1 / num_samples) * np.matmul(np.matmul(X_j, W), X_j)
-        #     # r = np.dot(LA.inv(W), y - y_est)
-        #     z_j = (1 / num_samples) * np.dot(X_j, y - y_est) + v_j * w[j]
-            
-        #     w[j] = soft_thresholding(z_j, alpha) / v_j
-            
-        #     if v_j > 0:
-        #         print(v_j)
-            
+        w_pseudo = self._coordinate_descent_least_squares(X_pseudo, y_pseudo, w_pseudo, init_alpha)
+        
+        # Update 
+        # TODO: use backtracking line search
+        w = 0.1 * w + 0.9 * w_pseudo
+        
         return w
+    
+    # @staticmethod
+    # def _coordinate_descent_step(X, y, w, alpha):
+    #     _, num_feats = X.shape
+        
+    #     for k in range(num_feats):
+    #         # use gradient descent to find the local minimum
+    #         for i in range(10):
+    #             proj = np.dot(X, w)
+                
+    #             f_prime = np.dot(X[:, k] ** 2 / (1 + proj), np.exp(proj) - 1 / (1 + proj))
+                
+    #             if f_prime == 0:
+    #                 break
+
+    #             f = np.dot(X[:, k] / (1 + proj), np.exp(proj)) - np.dot(y, X[:, k]) + alpha * np.sign(w[k])
+    #             w[k] -= f / f_prime
+            
+    #     return w
